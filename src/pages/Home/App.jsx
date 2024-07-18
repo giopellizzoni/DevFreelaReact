d56ec22989e7576d7d7d7d71e69178409c8c4abe
Giovanni Pellizzoni <pelissoni@gmail.com>
2024-07-18 13:47:13 +0200

    Finish Module

--- src/pages/Home/App.jsx
+++ src/pages/Home/App.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Button from "../../components/Button";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("Giovanni");
+
+  useEffect(() => {
+    setCount(1);
+  }, []);
+
+  //   useEffect(() => {
+  //     setTimeout(() => setCount(count + 1), 600)
+  //   });
+
+  useEffect(() => {
+    if (count % 2 === 0) {
+      setName("Numero é par");
+    } else {
+      setName("Numero é ímpar");
+    }
+  }, [count]);
+
+  useEffect(() => () => console.log("desmontar"));
+
+  const onClickButton = () => {
+    setName("Pellizzoni");
+    // setCount(count + 1);
+  };
+
   return (
-    <p>Texto na Tela</p>
+    <>
+      <p>{count}</p>
+      <p>{name}</p>
+      <Button onClick={onClickButton} />
+    </>
   );
 }
 
---
+++ src/pages/Home/AppClass.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Button from "../../components/Button";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  onClickButton = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <>
+        <p>{this.state.count}</p>
+        <Button onClick={this.onClickButton} />
+      </>
+    );
+  }
+}
+
+export default App;
