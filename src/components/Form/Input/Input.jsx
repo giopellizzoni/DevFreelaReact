e44367ae96a89914185e4eb4aee9d1ec9deba9f0
Giovanni Pellizzoni <pelissoni@gmail.com>
2024-07-19 16:31:54 +0200

    Validating Login

---
+++ src/components/Form/Input/Input.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { TextField, Typography } from "@mui/material";
+import { Field } from "formik";
+
+function Input(props) {
+  return (
+    <Field {...props}>
+      {({ field, meta }) => (
+        <div className="mb-1">
+          <TextField
+            label={props.label}
+            inputProps={{ ...field, ...props }}
+            className="w-100"
+            variant="standard"
+          />
+          {meta.touched &&
+            meta.error && (
+              <div className="p-relative">
+                <Typography
+                  className="p-absolute"
+                  variant="caption"
+                  color="error"
+                >
+                  {meta.error}
+                </Typography>
+              </div>
+            )}
+        </div>
+      )}
+    </Field>
+  );
+}
+
+export default Input;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,44 +1,69 @@
+import { Typography, Grid } from "@mui/material";
+import { Form, Formik } from "formik";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Typography, TextField } from "@mui/material";
-import Layout from "../../containers/Layout";
 import { ReactComponent as UserRegisterBG } from "../../assets/images/bg-register-user.svg";
-import Button from "../../components/Button";
 import { ROUTE_PATHS } from "../../commons/constants/routes-path";
+import Button from "../../components/Button";
+import Input from "../../components/Form/Input";
+import Layout from "../../containers/Layout";
+import { INITIAL_VALUES } from "./constants";
+import { validateForm } from "./schemes";
 
 function Login() {
   const navigate = useNavigate();
+
+  const onSubmit = () => {
+    navigate(ROUTE_PATHS.projects);
+  };
+
   return (
     <Layout image={UserRegisterBG}>
-      <div className="w-100 mb-3">
-        <Typography variant="h2" fontWeight="bold">
-          Entre ou faça seu cadastro
-        </Typography>
-      </div>
+      <Formik
+        onSubmit={onSubmit}
+        initialValues={INITIAL_VALUES}
+        validationSchema={validateForm}
+      >
+        {({ handleSubmit }) => (
+          <Form onSubmit={handleSubmit}>
+            <div className="w-100 mb-3">
+              <Typography variant="h2" fontWeight="bold">
+                Entre ou faça seu cadastro
+              </Typography>
+            </div>
 
-      <div className="w-75 mb-3">
-        <TextField className="w-100 mb-2" label="Email" variant="standard" />
-        <TextField className="w-100 mb-2" label="Senha" variant="standard" />
-      </div>
-      <div className="w-75 mb-3">
-        <Button
-          className="py-2 mb-3"
-          size="large"
-          fullWidth
-          onClick={() => navigate(ROUTE_PATHS.projects)}
-        >
-          Entrar
-        </Button>
-        <Button
-          className="py-2"
-          size="large"
-          fullWidth
-          variant="text"
-          onClick={() => navigate(`${ROUTE_PATHS.users}/new-user`)}
-        >
-          Cadastrar
-        </Button>
-      </div>
+            <div className="w-75 mb-3">
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <Input name="email" label="E-mail" />
+                </Grid>
+                <Grid item xs={12}>
+                  <Input name="password" label="Senha" type="password" />
+                </Grid>
+              </Grid>
+            </div>
+            <div className="w-75 mb-3">
+              <Button
+                className="py-2 mb-3"
+                size="large"
+                fullWidth
+                type="submit"
+              >
+                Entrar
+              </Button>
+              <Button
+                className="py-2"
+                size="large"
+                fullWidth
+                variant="text"
+                onClick={() => navigate(`${ROUTE_PATHS.users}/new-user`)}
+              >
+                Cadastrar
+              </Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </Layout>
   );
 }
